f69c08d4267324edfc941b6cc77845aead45a0af
Clyde Balneaves <tagusk@hotmail.com>
2012-03-27 10:20:14 +0100

    Added NuGet spec

--- src/PublicSuffix/Properties/AssemblyInfo.cs
+++ src/PublicSuffix/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PublicSuffix")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET domain name parser of the PublicSuffix.org list maintained by Mozilla")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("interlinkONE, Inc.")]
 [assembly: AssemblyProduct("PublicSuffix")]
